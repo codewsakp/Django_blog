3f2baab7a6f38ca4e302a11371d2440c451ec296
Sakshi Patil <sakshijpatil04@gmail.com>
2026-01-18 23:50:53 +0530

    post by categoru

--- blog_sys/settings.py
+++ blog_sys/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-snd*@d^+$75t_)44=ems(h$43c&e**h+@@r%@%y1k07$$!mldd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -62,6 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'blogs.context_processors.get_categories',
             ],
         },
     },
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+from django.http import HttpResponse
+
+from .models import Blog, Category
 
 # Create your views here.
+def posts_by_category(request, category_id ):
+    #fetch the posts that belongs to the category id
+
+    posts= Blog.objects.filter(status='Published', category=category_id)
+    # try except block if some category doen not exist
+    #try:
+    #    category = Category.objects.get(pk=category_id)
+    #except:
+        # redirect the user to the homepage if certain category does not exist
+    #    return redirect('home')
+    
+    # Use get_object_or_404 when you want to show just the 404 error page
+    category = get_object_or_404(Category, pk=category_id)
+
+    context={
+        'posts':posts,
+        'category': category,
+    }
+
+    return render(request, 'posts_by_category.html', context)

0f7d7bd609250d7b03ec38826afd688004593682
Sakshi Patil <sakshijpatil04@gmail.com>
2026-01-29 23:09:51 +0530

    Dashboard to add new post

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from blogs.models import Blog, Category
 from django.contrib.auth.decorators import login_required
-from .forms import CategoryForm
+from .forms import BlogPostForm, CategoryForm
 
 # Create your views here.
 
@@ -52,3 +52,19 @@ def delete_category(request,pk):
     category = get_object_or_404(Category, pk=pk)
     category.delete()
     return redirect('categories')
+
+
+def posts(request):
+    posts = Blog.objects.all()
+    context = {
+        'posts' : posts,
+    }
+    return render(request, 'dashboard/posts.html',context)
+
+
+def add_post(request):
+    form = BlogPostForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboard/add_post.html', context)
